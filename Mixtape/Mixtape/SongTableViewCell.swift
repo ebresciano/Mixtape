065405c6184230d70a835e1e079da1942bfaf022
Eva <Bresciano>
2016-07-12 18:24:09 -0600

    Everything is a disaster
    but my views are on the storyboard and my network call is getting there

---
+++ Mixtape/Mixtape/SongTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongTableViewCell.swift
+//  Mixtape
+//
+//  Created by Eva Marie Bresciano on 7/12/16.
+//  Copyright © 2016 Eva Bresciano. All rights reserved.
+//
+
+import UIKit
+
+class SongTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
