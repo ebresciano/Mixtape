8418d8772f5e70e207d6c9a09bdfe98ad90107a4
Eva <Bresciano>
2016-07-20 19:39:10 -0600

    Create song function
    Create song function and sucessfully fetching and displaying album art

---
+++ Mixtape/Mixtape/SongSearchResultsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongSearchResultsTableViewCell.swift
+//  Mixtape
+//
+//  Created by Eva Marie Bresciano on 7/20/16.
+//  Copyright © 2016 Eva Bresciano. All rights reserved.
+//
+
+import UIKit
+
+class SongSearchResultsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
